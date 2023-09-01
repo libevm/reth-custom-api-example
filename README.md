c3a55093c30db55e994fbf24b39d93c9ff222d96
libevm <libevm@proton.me>
2023-08-31 23:30:13 -0700

    tests in rust woot

--- README.md
+++ README.md
@@ -15,51 +15,7 @@ cargo build --release
 ```
 
 # Test
-TODO, add test in Rust
 
-```javascript
-const { ethers } = require("ethers");
-const { parseUnits } = require("ethers/lib/utils");
-
-const main = async () => {
-  const provider = new ethers.providers.JsonRpcProvider(
-    "http://127.0.0.1:8545",
-    1337
-  );
-  const wallet = new ethers.Wallet(
-    "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d"
-  ).connect(provider);
-
-  const bribe = parseUnits("0.01");
-  const gasLimit = 21000;
-  const ethPerGas = bribe.div(gasLimit);
-
-  const walletAddr = await wallet.getAddress();
-
-  const txReq = {
-    from: walletAddr,
-    to: walletAddr,
-    gasLimit: gasLimit,
-    maxFeePerGas: ethPerGas,
-    maxPriorityFeePerGas: ethPerGas,
-    type: 2,
-    chainId: 1,
-  };
-
-  let tx;
-  for (let i = 0; i < 6; i++) {
-    tx = await wallet.sendTransaction({
-      gasPrice: parseUnits("15", 9),
-      to: wallet.address,
-    });
-  }
-  await tx.wait();
-
-  const bn = await provider.getBlockNumber();
-
-  const resp = await provider.send("eth_getGasUsedByBlock", [
-    "0x" + bn.toString(16),
-  ]);
-  console.log("gasUsed", resp);
-}
+```
+cargo test
 ```
--- src/apis/gasused.rs
+++ src/apis/gasused.rs
@@ -28,25 +28,7 @@ where
 {
     fn get_gas_used_by_block(&self, bn: BlockNumberOrTag) -> RpcResult<u64> {
         match self.provider.block_by_number_or_tag(bn) {
-            Ok(Some(b)) => match self.provider.receipts_by_block(b.number.into()) {
-                Ok(Some(txs)) => {
-                    let mut gas = 0;
-                    for tx in txs {
-                        if tx.cumulative_gas_used > gas {
-                            gas = tx.cumulative_gas_used;
-                        }
-                    }
-                    Ok(gas)
-                }
-                _ => {
-                    error!("failed to retrieve transactions for bn {bn:?}");
-                    Err(ErrorObjectOwned::owned(
-                        -1,
-                        "Failed to retrieve transactions",
-                        None::<()>,
-                    ))
-                }
-            },
+            Ok(Some(b)) => Ok(b.gas_used),
             _ => {
                 error!("unable to retrieve block {bn:?}");
                 Err(ErrorObjectOwned::owned(
@@ -56,5 +38,37 @@ where
                 ))
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{CustomEthNamespaceExt, CustomEthNamespaceServer};
+    use reth_primitives::Block;
+    use reth_primitives::Header;
+    use reth_provider::test_utils::MockEthProvider;
+
+    #[tokio::test]
+    async fn test_get_gas_used() {
+        let mock_provider = MockEthProvider::default();
+
+        // Insert it into the mock provider
+        let block = Block {
+            header: Header {
+                number: 42,
+                gas_used: 42069,
+                ..Default::default()
+            },
+            ..Default::default()
+        };
+        let block_hash = block.hash_slow();
+        mock_provider.add_block(block_hash, block);
+
+        let ext = CustomEthNamespaceExt::new(mock_provider);
+        let gas_used = ext.get_gas_used_by_block(42.into()).unwrap();
+        let err_found = ext.get_gas_used_by_block(43.into());
+
+        assert_eq!(gas_used, 42069);
+        assert!(err_found.is_err());
     }
 }
